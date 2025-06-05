1e8cb7e24a4d6e5d68f1de08a461480c8a31ae7c
Thomas-Galbignani <thomas.lino.galbignani@gmail.com>
2025-06-05 16:51:30 +0200

    noExtra

--- scripts/D3.js
+++ scripts/D3.js
@@ -237,11 +237,22 @@ console.log(crewMass1);
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
 
+console.log(starWarsCharacters);
+
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  if (starWarsCharacters[i].gender === `n/a`) {
+    starWarsCharacters[i].gender = `robot`;
+  }
+}
+console.log(starWarsCharacters);
+
 /* --EXTRA-- ESERCIZIO 9
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
   Una volta fatto crea un console.log per controllare la proprietà length di "charactersNames" prima e dopo l'operazione.
 */
+console.log(`prima`, starWarsCharacters.length);
 
+console.log(`dopo`, starWarsCharacters.length);
 /* --EXTRA-- ESERCIZIO 10
   Crea una funzionalità che selezioni un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
 */
